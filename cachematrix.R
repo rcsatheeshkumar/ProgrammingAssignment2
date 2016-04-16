74120a44c85a42916a4bc1bd69c8191f4fb020ca
rcsatheeshkumar <rcsatheeshkumar@gmail.com>
2016-04-17 00:23:12 +0530

    programmingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,5 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## returns a special vector with the list of functions to 
+## get and set the vector values and also to store and retrieve the mean  
 
 makeCacheMatrix <- function(x = matrix()) {
 inverse<-NULL
